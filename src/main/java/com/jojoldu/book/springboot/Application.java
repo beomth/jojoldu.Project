3afdae232e75a1b9080d2c015739a5ef2e190a9e
beomth <xhxkf0202@naver.com>
2023-07-05 01:54:20 +0900

    main

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 // 특히, @SpringBootApplication이 있는 위치부터 설정을 읽어가기 때문에, 이 클래스는 항상 프로젝트 최상단에 있어야 함.
 public class Application {
     public static void main(String args[]){
+
         SpringApplication.run(Application.class, args);
     }
 }
